5273db7c3179abc71cfd20409d85c79ec325ee83
BuddyVolly <andreasvollrath@gmx.de>
2020-11-16 13:56:06 +0100

    debugging install file

--- install_ost.sh
+++ install_ost.sh
@@ -4,7 +4,7 @@
 apt-get -y install python3-pip git libgdal-dev python3-gdal libspatialindex-dev wget libgfortran3 imagemagick file
 
 # set Snap and OTB version and download link
-OTB_VERSION="7.1.0"
+OTB_VERSION="7.2.0"
 TBX_VERSION="7"
 TBX_SUBVERSION="0"
 SNAP_URL="http://step.esa.int/downloads/${TBX_VERSION}.${TBX_SUBVERSION}/installers"
@@ -13,7 +13,7 @@ TBX="esa-snap_sentinel_unix_${TBX_VERSION}_${TBX_SUBVERSION}.sh"
 # get Snap
 wget "${SNAP_URL}/${TBX}"
 chmod 755 ${TBX}
-wget https://github.com/ESA-PhiLab/OpenSarToolkit/blob/merge_split_godale/snap7.varfile
+wget https://raw.githubusercontent.com/ESA-PhiLab/OpenSarToolkit/master/snap7.varfile
 
 # install snap
 ./${TBX} -q -varfile snap7.varfile
@@ -23,7 +23,7 @@ rm ${TBX}
 OTB=OTB-${OTB_VERSION}-Linux64.run
 wget https://www.orfeo-toolbox.org/packages/${OTB}
 chmod +x $OTB
-./$ {OTB}
+./${OTB}
 rm -f ${OTB}
 
 # install ost
