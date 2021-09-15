eb16e46260127e01eddc1127e8175a75b3d79557
BuddyVolly <andreasvollrath@gmx.de>
2021-09-15 16:44:43 +0200

    Update install_ost.sh

--- install_ost.sh
+++ install_ost.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 # get ubuntu dependencies
-apt-get -y install python3-pip git libgdal-dev python3-gdal libspatialindex-dev wget libgfortran3 imagemagick file
+apt-get -y install python3-pip git libgdal-dev python3-gdal libspatialindex-dev wget libgfortran5 imagemagick file
 
 # set Snap and OTB version and download link
-OTB_VERSION="7.3.0"
-TBX_VERSION="7"
+OTB_VERSION="7.4.0"
+TBX_VERSION="8"
 TBX_SUBVERSION="0"
 SNAP_URL="http://step.esa.int/downloads/${TBX_VERSION}.${TBX_SUBVERSION}/installers"
 TBX="esa-snap_sentinel_unix_${TBX_VERSION}_${TBX_SUBVERSION}.sh"
@@ -18,6 +18,12 @@ wget https://raw.githubusercontent.com/ESA-PhiLab/OpenSarToolkit/master/snap7.va
 # install snap
 ./${TBX} -q -varfile snap7.varfile
 rm ${TBX}
+
+# update snap
+/home/ost/programs/snap/bin/snap --nosplash --nogui --modules --update-all 2>&1 | while read -r line; do \
+  echo "$line" && \
+  [ "$line" = "updates=0" ] && sleep 2 && pkill -TERM -f "snap/jre/bin/java"; \
+done; exit 0
 
 # OTB installation
 OTB=OTB-${OTB_VERSION}-Linux64.run
